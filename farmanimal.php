842f42259bb3c756eba052faa83a4e7981c73e0d
tester guy <testerguy@users.noreply.github.com>
2014-12-23 17:23:58 -0800

    first commit

---
+++ farmanimal.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST[animal]))
+{
+$animal=$_POST[animal];
+$a=array("cat","dog","cow","horse","sheep","goat","chicken","pig","horse");
+
+if (in_array((strtolower($animal)),$a))
+  {
+  echo "a $animal is a farm animal";
+  }
+else
+  {
+  echo "a $animal is not a farm animal";
+  } 
+}
+else
+{
+echo "<form action='document.php' method='post'>Type an animal name: <input type='text' name='animal'/><input type='submit' value='Submit'/></form>";
+}
+?>

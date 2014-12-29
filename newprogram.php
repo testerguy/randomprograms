1406eea5b1ad69dc8cea9e8b33209a0e4da9bf93
tester guy <testerguy@users.noreply.github.com>
2014-12-29 14:11:53 -0800

    new program created

---
+++ newprogram.php
@@ -0,0 +1,26 @@
+<?php
+
+
+echo <<<EOD
+
+<input type="text" id="yeah" />
+
+<input type="submit" id="submit"/>
+<div id="output"></div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function() {
+  $("#submit").onclick(function(){
+    $("#output").text=$("#yeah").html();
+    });
+  });
+
+
+</script>
+
+EOD;
+
+
+?>

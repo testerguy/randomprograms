af39e84a9674e08c3a21a1c7989d04db124a5f02
tester guy <testerguy@users.noreply.github.com>
2014-12-29 15:32:03 -0800

    optimization

--- newprogram.php
+++ newprogram.php
@@ -1,19 +1,19 @@
 <?php
 
-
 echo <<<EOD
 
 <input type="text" id="yeah" />
 
-<input type="submit" id="submit"/>
-<div>Mirror: <span id="output"></span></div>
+<!--<input type="submit" id="submit"/>-->
+<div>Mirror: <span id="output"> </span></div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script type="text/javascript">
 
 $(document).ready(function() {
-  $("#submit").onclick(function(){
-    $("#output").text=$("#yeah").html();
+  $("#yeah").keyup(function(){
+    var newvalue=$("#yeah").val();
+    $("#output").text(newvalue);
     });
   });
 
